8aab22071606c4f4add3d3f9891766d604d3746e
Leandro Diaz <leandrodiaz042@gmail.com>
2024-08-23 14:23:10 -0300

    update: using form data with ref

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -3,13 +3,17 @@
 
 import * as React from 'react'
 
-function UsernameForm({onSubmitUsername}) {
+function UsernameForm({ onSubmitUsername }) {
   // 🐨 add a submit event handler here (`handleSubmit`).
   // 💰 Make sure to accept the `event` as an argument and call
   // `event.preventDefault()` to prevent the default behavior of form submit
   // events (which refreshes the page).
   // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  //
+  const userNameRef = React.useRef(null)
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    onSubmitUsername(userNameRef.current.value)
+  }
   // 🐨 get the value from the username input (using whichever method
   // you prefer from the options mentioned in the instructions)
   // 💰 For example: event.target.elements[0].value
@@ -20,10 +24,10 @@ function UsernameForm({onSubmitUsername}) {
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor='userName'>Username:</label>
+        <input ref={userNameRef} id='userName' type="text" />
       </div>
       <button type="submit">Submit</button>
     </form>
